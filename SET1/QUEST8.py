52a605848bb22315392a2c6770db4db6dd3bd96c
Matteo Ruggiero <kirito.poppox@gmail.com>
2021-05-09 12:00:31 +0200

    Add files via upload

---
+++ SET1/QUEST8.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  paymentstrictSRT.py
+#  
+#  Copyright 2021 mRuggi <mRuggi@PC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+from stellar_sdk import Keypair,Server,Network,TransactionBuilder,Asset
+
+SRT=Asset("SRT","GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B")
+XLM=Asset.native()
+keypair=Keypair.from_secret("SCQJ65ZH5UO35HQ4IRG545F22LJ37Q44G3UDSYFLRACTWJKCULEMC2S3")
+print(keypair.public_key)
+server = Server(horizon_url="https://horizon-testnet.stellar.org")
+
+path=[]
+
+
+
+tx= (
+	TransactionBuilder(
+		source_account = server.load_account(account_id=keypair.public_key), 
+		network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE, 
+		base_fee=100) 
+		.append_change_trust_op(SRT.code,SRT.issuer)
+		.append_path_payment_strict_send_op(SRT.issuer,XLM.code,XLM.issuer,"1",SRT.code,SRT.issuer,"1",path)
+		.build()
+)
+tx.sign(keypair.secret)
+response = server.submit_transaction(tx)
+print("\nTransaction hash: {}".format(response["hash"]))
+print("Premi un tasto per continuare")
+input()
